a0198fe721618a3cf1267b944247a63f783e6c69
Lars Frogner <lars_frogner@hotmail.com>
2021-07-20 16:18:11 +0200

    Move password-related functions to separate file

---
+++ site/src/security.php
@@ -0,0 +1,24 @@
+<?php
+require_once('database.php');
+
+function hashPassword($password) {
+  return password_hash($password, PASSWORD_DEFAULT);
+}
+
+function createPasswordTableIfMissing($database, $hash_len) {
+  $hash_len = strval($hash_len);
+  createTableIfMissing($database, 'password', array('id' => 'TINYINT UNSIGNED PRIMARY KEY', 'hash' => "CHAR($hash_len) NOT NULL"));
+}
+
+function storeHashedPassword($database, $hashed_password) {
+  writeValuesToTable($database, 'password', array('id' => 0, 'hash' => $hashed_password));
+}
+
+function readHashedPassword($database) {
+    $result = readValuesFromTable($database, 'password', 'hash', 'id = 0');
+    if (empty($result)) {
+        echo "Password hash not present in database\n";
+        exit(1);
+    }
+    return $result[0]['hash'];
+}
