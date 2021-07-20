4329c710400768d70e2850f1771e4b81dc056f05
Lars Frogner <lars_frogner@hotmail.com>
2021-07-21 01:26:06 +0200

    Remove unnecessary tags

--- site/templates/navbar.php
+++ site/templates/navbar.php
@@ -1,14 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand">Babymonitor</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-      aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link<?php echo (LOCATION == 'main' ? ' active" aria_current="page' : '') ?>" href="main.php">Moduser</a>
-        <a class="nav-link<?php echo (LOCATION == 'settings' ? ' active" aria_current="page': '') ?>" href="settings.php">Innstillinger</a>
+        <a class="nav-link<?php echo LOCATION == 'main' ? ' active' : '' ?>" href="main.php">Moduser</a>
+        <a class="nav-link<?php echo LOCATION == 'settings' ? ' active' : '' ?>" href="settings.php">Innstillinger</a>
         <a class="nav-link" href="logout.php">Logg ut</a>
       </div>
     </div>
