ef46338f0b21f097f7a4a192445e15d3f3a20cfe
Lars Frogner <lars_frogner@hotmail.com>
2021-07-22 12:19:02 +0200

    Handle display of error messages from server

--- site/public/js/main.js
+++ site/public/js/main.js
@@ -1,18 +1,19 @@
-const mode_radio_ids = ['mode_radio_listen', 'mode_radio_audio', 'mode_radio_video', 'mode_radio_standby'];
-const mode_content_ids = ['mode_content_listen', 'mode_content_audio', 'mode_content_video', 'mode_content_standby'];
+const mode_radio_ids = ['mode_radio_standby', 'mode_radio_listen', 'mode_radio_audio', 'mode_radio_video'];
+const mode_content_ids = ['mode_content_standby', 'mode_content_listen', 'mode_content_audio', 'mode_content_video'];
 const waiting_content_id = 'mode_content_waiting';
+const error_content_id = 'mode_content_error';
+const error_content_message_id = 'mode_content_error_message';
 
 function requestModeChange(radio) {
     var data = new URLSearchParams();
-    data.append('checked_radio_id', radio.id);
+    data.append('requested_mode', radio.value);
 
     fetch('change_mode.php', {
         method: 'post',
         body: data
     })
-        .then(response => {
-            return handleModeChangeResponse(radio.id, response);
-        })
+        .then(response => response.text())
+        .then(response_text => handleModeChangeResponse(radio.id, response_text))
         .catch(error => {
             console.log(error)
         });
@@ -25,9 +26,18 @@ function indicateWaiting() {
     setVisibleContent(waiting_content_id);
 }
 
-function handleModeChangeResponse(checked_radio_id, response) {
-    setVisibleContent(getContentIdByRadioId(checked_radio_id));
-    setDisabledForModeRadios(false);
+function handleModeChangeResponse(checked_radio_id, response_text) {
+    console.log(response_text);
+    switch (response_text) {
+        case '0':
+            setVisibleContent(getContentIdByRadioId(checked_radio_id));
+            setDisabledForModeRadios(false);
+            break;
+        default:
+            document.getElementById(error_content_message_id).innerHTML = response_text;
+            setVisibleContent(error_content_id);
+            break;
+    }
 }
 
 function setDisabledForModeRadios(is_disabled) {
@@ -37,6 +47,12 @@ function setDisabledForModeRadios(is_disabled) {
 }
 
 function setVisibleContent(visible_content_id) {
+    const error_content = document.getElementById(error_content_id);
+    if (visible_content_id == error_content_id) {
+        showElement(error_content);
+    } else {
+        hideElement(error_content);
+    }
     const waiting_content = document.getElementById(waiting_content_id);
     if (visible_content_id == waiting_content_id) {
         showElement(waiting_content);
