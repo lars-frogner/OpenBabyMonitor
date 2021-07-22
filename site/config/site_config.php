f529cc9ffbbc6d46cc859091b706aa19d5ebc671
Lars Frogner <lars_frogner@hotmail.com>
2021-07-22 12:25:07 +0200

    Add import of control script

--- site/config/site_config.php
+++ site/config/site_config.php
@@ -5,6 +5,7 @@ require_once('database_config.php');
 require_once('mode_config.php');
 require_once(SRC_PATH . '/security.php');
 require_once(SRC_PATH . '/session.php');
+require_once(SRC_PATH . '/control.php');
 
 switch (basename($_SERVER['SCRIPT_NAME'])) {
   case 'main.php':
