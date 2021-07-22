189284cfd08b7217fccb3f54ff7c858db615ed33
Lars Frogner <lars_frogner@hotmail.com>
2021-07-22 13:18:31 +0200

    Add additional setup

--- setup.sh
+++ setup.sh
@@ -21,21 +21,45 @@ set -e
 # Change password for pi user and set hostname to "babymonitor", set time zone to Oslo and enable camera module.
 # ssh pi@babymonitor # Log in with new password
 # Note: mini USB microphone does not appear to useable without sudo for other users than pi.
+# Clone the babymonitor repository:
+# git clone https://github.com/lars-frogner/babymonitor.git
+# Run this setup script (as pi user).
 
-SITE_ROOT=~/babymonitor/site/public
+BM_PATH=$(dirname $(readlink -f $0))
+BM_ENV_PATH=$BM_PATH/babymonitor_env.sh
+BM_SITE_ROOT=/var/www/babymonitor
+BM_LINKED_SITE_ROOT=$BM_PATH/site/public
+BM_PICAM_PATH=$BM_PATH/picam
+BM_PICAM_LOG_PATH=/var/log/picam.log
+BM_SHAREDMEM_PATH=/run/shm
+BM_PICAM_OUTPUT_PATH=$BM_SHAREDMEM_PATH/hls
+BM_PICAM_LINKED_OUTPUT_PATH=$BM_LINKED_SITE_ROOT/hls
 
-UPDATE=false
+SETUP_ENV=true
+if [[ "$UPDATE" = true ]]; then
+    touch $BM_ENV_PATH
+    echo -e "export BM_PATH=$BM_PATH\n" >> $BM_ENV_PATH
+    echo -e "export BM_SITE_ROOT=$BM_SITE_ROOT\n" >> $BM_ENV_PATH
+    echo -e "export BM_LINKED_SITE_ROOT=$BM_LINKED_SITE_ROOT\n" >> $BM_ENV_PATH
+    echo -e "export BM_PICAM_PATH=$BM_PICAM_PATH\n" >> $BM_ENV_PATH
+    echo -e "export BM_PICAM_LOG_PATH=$BM_PICAM_LOG_PATH\n" >> $BM_ENV_PATH
+    echo -e "export BM_SHAREDMEM_PATH=$BM_SHAREDMEM_PATH\n" >> $BM_ENV_PATH
+    echo -e "export BM_PICAM_OUTPUT_PATH=$BM_PICAM_OUTPUT_PATH\n" >> $BM_ENV_PATH
+    echo -e "export BM_PICAM_LINKED_OUTPUT_PATH=$BM_PICAM_LINKED_OUTPUT_PATH\n" >> $BM_ENV_PATH
+fi
+
+UPDATE=true
 if [[ "$UPDATE" = true ]]; then
     sudo apt -y update
     sudo apt -y dist-upgrade
 fi
 
-SETUP_BASH_CONFIG=false
+SETUP_BASH_CONFIG=true
 if [[ "$SETUP_BASH_CONFIG" = true ]]; then
     # Enable convenient ls aliases
-    sudo -u admin sed -i "s/#alias ll='ls -l'/alias ll='ls -lh'/g" ~/.bashrc
-    sudo -u admin sed -i "s/#alias la='ls -A'/alias la='ls -A'/g" ~/.bashrc
-    sudo -u admin sed -i "s/#alias l='ls -CF'/alias l='ls -Alh'/g" ~/.bashrc
+    sed -i "s/#alias ll='ls -l'/alias ll='ls -lh'/g" ~/.bashrc
+    sed -i "s/#alias la='ls -A'/alias la='ls -A'/g" ~/.bashrc
+    sed -i "s/#alias l='ls -CF'/alias l='ls -Alh'/g" ~/.bashrc
 
     # Add alias for showing CPU temperature
     echo -e "\nalias temp='sudo vcgencmd measure_temp'\n" >> ~/.bashrc
@@ -45,11 +69,10 @@ if [[ "$SETUP_BASH_CONFIG" = true ]]; then
     sed -i 's/# "\\e\[B": history-search-forward/"\\e[B": history-search-forward/g' ~/.inputrc
     sed -i 's/# "\\e\[A": history-search-backward/"\\e[A": history-search-backward/g' ~/.inputrc
 
-    touch ~/monitor_env.sh
-    echo -e "\nsource ~/monitor_env.sh\n" >> ~/.bashrc
+    echo -e "\nsource $BM_ENV_PATH\n" >> ~/.bashrc
 fi
 
-SETUP_STATIC_IP=false
+SETUP_STATIC_IP=true
 if [[ "$SETUP_STATIC_IP" = true ]]; then
     HOST_IP=$(ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)
 
@@ -66,60 +89,61 @@ if [[ "$SETUP_STATIC_IP" = true ]]; then
 
     # Set static IP to current IP in dhcpcd.conf
     echo "
-    interface wlan0
-    static ip_address=$HOST_IP/24
-    static routers=$ROUTER_IP
-    static domain_name_servers=$DNS_IP" | sudo tee -a /etc/dhcpcd.conf
+interface wlan0
+static ip_address=$HOST_IP/24
+static routers=$ROUTER_IP
+static domain_name_servers=$DNS_IP" | sudo tee -a /etc/dhcpcd.conf
 fi
 
-DISABLE_BLUETOOTH=false
+DISABLE_BLUETOOTH=true
 if [[ "$DISABLE_BLUETOOTH" = true ]]; then
     # Disable bluetooth
     echo "
 
-    # Disable Bluetooth
-    dtoverlay=disable-bt" | sudo tee -a /boot/config.txt
+# Disable Bluetooth
+dtoverlay=disable-bt" | sudo tee -a /boot/config.txt
 
     set +e
     sudo systemctl disable hciuart.service
     sudo systemctl disable bluetooth.service
 fi
 
-DISABLE_IPV6=false
+DISABLE_IPV6=true
 if [[ "$DISABLE_IPV6" = true ]]; then
     # Disable IPv6 hostname resolution
     sudo sed -i 's/use-ipv6=yes/use-ipv6=no/g' /etc/avahi/avahi-daemon.conf
 fi
 
-INSTALL_SERVER=false
+INSTALL_SERVER=true
 if [[ "$INSTALL_SERVER" = true ]]; then
-    # Install Apache and PHP
-    sudo apt -y install apache2 libapache2-mod-php
+    # Install Apache, PHP and MySQL (MariaDB)
+    sudo apt -y install apache2 mariadb-server php php-mysql libapache2-mod-php
+
+    # Configure MySQL
+    sudo mysql_secure_installation
 
     sudo adduser pi www-data
     sudo adduser pi audio
-    touch /var/log/picam.log
-    chmod -R 755 $SITE_ROOT
-    sudo ln -s $SITE_ROOT /var/www/babymonitor
+    chmod -R 755 $LINKED_SITE_ROOT
+    sudo ln -s $LINKED_SITE_ROOT $SITE_ROOT
 fi
 
-INSTALL_PICAM=false
+INSTALL_PICAM=true
 if [[ "$INSTALL_PICAM" = true ]]; then
     # Install dependencies
     sudo apt -y install libharfbuzz0b libfontconfig1
 
     # Create directories and symbolic links
-    DEST_DIR=~/picam
-    SHM_DIR=/run/shm
+    sudo install -d -o pi -g pi -m 755 $PICAM_PATH{,/archive} $BM_SHAREDMEM_PATH/{rec,hooks,state}
 
-    sudo install -d -o pi -g pi -m 755 $DEST_DIR{,/archive} $SHM_DIR/{rec,hooks,state}
+    ln -sfn {$PICAM_PATH,$BM_SHAREDMEM_PATH/rec}/archive
+    ln -sfn {$BM_SHAREDMEM_PATH,$PICAM_PATH}/rec
+    ln -sfn {$BM_SHAREDMEM_PATH,$PICAM_PATH}/hooks
+    ln -sfn {$BM_SHAREDMEM_PATH,$PICAM_PATH}/state
 
-    ln -sfn {$DEST_DIR,$SHM_DIR/rec}/archive
-    ln -sfn {$SHM_DIR,$DEST_DIR}/rec
-    ln -sfn {$SHM_DIR,$DEST_DIR}/hooks
-    ln -sfn {$SHM_DIR,$DEST_DIR}/state
+    sudo ln -s $BM_PICAM_OUTPUT_PATH $BM_PICAM_LINKED_OUTPUT_PATH
 
-    sudo ln -s $SHM_DIR/hls /var/www/babymonitor/hls
+    touch $PICAM_LOG_PATH
 
     # Install picam binary
     PICAM_VERSION=1.4.9
@@ -133,17 +157,16 @@ if [[ "$INSTALL_PICAM" = true ]]; then
     FILENAME_ROOT="${FILENAME%%.*}"
     wget $DOWNLOAD_URL
     tar xvf $FILENAME
-    mv $FILENAME_ROOT/picam $DEST_DIR/
+    mv $FILENAME_ROOT/picam $PICAM_PATH/
     rm -r $FILENAME $FILENAME_ROOT
 fi
 
-SETUP_MIC=false
+SETUP_MIC=true
 if [[ "$SETUP_MIC" = true ]]; then
-    # Setup environment variables
-    echo -e "export MIC_ID=$(arecord -l | perl -n -e'/^card (\d+):.+, device (\d):.+$/ && print "hw:$1,$2"')\n" >> ~/monitor_env.sh
+    echo -e "export BM_MIC_ID=$(arecord -l | perl -n -e'/^card (\d+):.+, device (\d):.+$/ && print "hw:$1,$2"')\n" >> $BM_ENV_PATH
 fi
 
-INSTALL_BOOTSTRAP=false
+INSTALL_BOOTSTRAP=true
 if [[ "$INSTALL_BOOTSTRAP" = true ]]; then
     sudo apt -y install unzip
 
@@ -160,7 +183,7 @@ if [[ "$INSTALL_BOOTSTRAP" = true ]]; then
     rm $FILENAME
 fi
 
-INSTALL_VIDEOJS=false
+INSTALL_VIDEOJS=true
 if [[ "$INSTALL_VIDEOJS" = true ]]; then
     sudo apt -y install unzip
 
@@ -175,4 +198,57 @@ if [[ "$INSTALL_VIDEOJS" = true ]]; then
     wget $DOWNLOAD_URL
     unzip $FILENAME -d $SITE_ROOT/library/video-js
     rm $FILENAME
+fi
+
+SETUP_SERVICES=true
+if [[ "$SETUP_SERVICES" = true ]]; then
+    USER=www-data
+    UNIT_DIR=/lib/systemd/system
+    LINKED_UNIT_DIR=$BM_PATH/control/services
+    SYSTEMCTL=/usr/bin/systemctl
+
+    mkdir -p $LINKED_UNIT_DIR
+
+    STARTUP_SERVICE_FILENAME=bm_startup.service
+    echo "[Unit]
+Description=Babymonitor startup script
+After=mysqld.service
+
+[Service]
+Type=oneshot
+User=$USER
+Group=$USER
+ExecStart=$BM_PATH/control/startup.py
+
+[Install]
+WantedBy=multi-user.target" > $LINKED_UNIT_DIR/$STARTUP_SERVICE_FILENAME
+
+    sudo ln -sfn {$LINKED_UNIT_DIR,$UNIT_DIR}/$STARTUP_SERVICE_FILENAME
+
+    sudo systemctl enable $STARTUP_SERVICE_FILENAME
+
+    CMD_ALIAS='Cmnd_Alias BM_MODES ='
+
+    for SERVICE in standby listen audiostream videostream
+    do
+        SERVICE_FILENAME=bm_$SERVICE.service
+        echo "[Unit]
+Description=Babymonitor $SERVICE service
+
+[Service]
+Type=simple
+User=$USER
+Group=$USER
+ExecStart=$BM_PATH/control/$SERVICE.py" > $LINKED_UNIT_DIR/$SERVICE_FILENAME
+
+        sudo ln -sfn {$LINKED_UNIT_DIR,$UNIT_DIR}/$SERVICE_FILENAME
+
+        $CMD_ALIAS+=" $SYSTEMCTL stop $SERVICE_FILENAME, $SYSTEMCTL start $SERVICE_FILENAME, $SYSTEMCTL restart $SERVICE_FILENAME,"
+    done
+    echo -e "${CMD_ALIAS%,}\n%$USER ALL=BM_MODES\n" | sudo tee /etc/sudoers.d/$USER
+fi
+
+INITIALIZE_DATABASE=true
+if [[ "$INITIALIZE_DATABASE" = true ]]; then
+    $BM_PATH/site/config/init/init_database.sh
 fi
