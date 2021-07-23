e16c4a32d26d7770da103322161585eba70381b2
Lars Frogner <lars_frogner@hotmail.com>
2021-07-23 20:22:09 +0200

    Improve setup

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ set -e
 # }
 # """
 # in boot directory of flashed SD card.
-# ssh pi@raspberrypi.home # Password is raspberry
+# ssh pi@raspberrypi.home # Password is raspberry (try .local if .home doesn't work)
 # echo -e "\nexport LC_ALL=en_GB.UTF-8\nexport LANGUAGE=en_GB.UTF-8\n" >> ~/.bashrc
 # sudo raspi-config
 # Change password for pi user and set hostname to "babymonitor", set time zone to Oslo and enable camera module.
@@ -24,6 +24,14 @@ set -e
 # Clone the babymonitor repository:
 # git clone https://github.com/lars-frogner/babymonitor.git
 # Run this setup script (as pi user).
+# Run configure_network.sh to generate network configurations for using the unit as a wireless
+# access point and for connecting to an existing wireless network as a client (the script works
+# for Raspbian Buster). The two modes of operation can be switched between using the
+# activate_ap_mode.sh and activate_client_mode.sh scripts.
+
+USER=pi
+GROUP=www-data
+PERMISSIONS=750
 
 BM_PATH=$(dirname $(readlink -f $0))
 BM_ENV_PATH=$BM_PATH/babymonitor_env.sh
@@ -36,16 +44,16 @@ BM_PICAM_OUTPUT_PATH=$BM_SHAREDMEM_PATH/hls
 BM_PICAM_LINKED_OUTPUT_PATH=$BM_LINKED_SITE_ROOT/hls
 
 SETUP_ENV=true
-if [[ "$UPDATE" = true ]]; then
+if [[ "$SETUP_ENV" = true ]]; then
     touch $BM_ENV_PATH
-    echo -e "export BM_PATH=$BM_PATH\n" >> $BM_ENV_PATH
-    echo -e "export BM_SITE_ROOT=$BM_SITE_ROOT\n" >> $BM_ENV_PATH
-    echo -e "export BM_LINKED_SITE_ROOT=$BM_LINKED_SITE_ROOT\n" >> $BM_ENV_PATH
-    echo -e "export BM_PICAM_PATH=$BM_PICAM_PATH\n" >> $BM_ENV_PATH
-    echo -e "export BM_PICAM_LOG_PATH=$BM_PICAM_LOG_PATH\n" >> $BM_ENV_PATH
-    echo -e "export BM_SHAREDMEM_PATH=$BM_SHAREDMEM_PATH\n" >> $BM_ENV_PATH
-    echo -e "export BM_PICAM_OUTPUT_PATH=$BM_PICAM_OUTPUT_PATH\n" >> $BM_ENV_PATH
-    echo -e "export BM_PICAM_LINKED_OUTPUT_PATH=$BM_PICAM_LINKED_OUTPUT_PATH\n" >> $BM_ENV_PATH
+    echo "export BM_PATH=$BM_PATH" >> $BM_ENV_PATH
+    echo "export BM_SITE_ROOT=$BM_SITE_ROOT" >> $BM_ENV_PATH
+    echo "export BM_LINKED_SITE_ROOT=$BM_LINKED_SITE_ROOT" >> $BM_ENV_PATH
+    echo "export BM_PICAM_PATH=$BM_PICAM_PATH" >> $BM_ENV_PATH
+    echo "export BM_PICAM_LOG_PATH=$BM_PICAM_LOG_PATH" >> $BM_ENV_PATH
+    echo "export BM_SHAREDMEM_PATH=$BM_SHAREDMEM_PATH" >> $BM_ENV_PATH
+    echo "export BM_PICAM_OUTPUT_PATH=$BM_PICAM_OUTPUT_PATH" >> $BM_ENV_PATH
+    echo "export BM_PICAM_LINKED_OUTPUT_PATH=$BM_PICAM_LINKED_OUTPUT_PATH" >> $BM_ENV_PATH
 fi
 
 UPDATE=true
@@ -69,101 +77,25 @@ if [[ "$SETUP_BASH_CONFIG" = true ]]; then
     sed -i 's/# "\\e\[B": history-search-forward/"\\e[B": history-search-forward/g' ~/.inputrc
     sed -i 's/# "\\e\[A": history-search-backward/"\\e[A": history-search-backward/g' ~/.inputrc
 
-    echo -e "\nsource $BM_ENV_PATH\n" >> ~/.bashrc
-fi
-
-SETUP_STATIC_IP=true
-if [[ "$SETUP_STATIC_IP" = true ]]; then
-    HOST_IP=$(ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)
-
-    ROUTER_IP=$(ip r | grep default | sed -n "s/^default via \([0-9\.]*\).*$/\1/p")
-    DNS_IP=$(sudo sed -n "s/^nameserver \([0-9\.]*\).*$/\1/p" /etc/resolv.conf | head -n 1)
-
-    # Check if dhcpcd service is running
-    if [[ -z $(systemctl | grep dhcpcd.service) ]]
-    then
-        # If not, start it
-        sudo service dhcpcd start
-        sudo systemctl enable dhcpcd
-    fi
-
-    # Set static IP to current IP in dhcpcd.conf
-    echo "
-interface wlan0
-static ip_address=$HOST_IP/24
-static routers=$ROUTER_IP
-static domain_name_servers=$DNS_IP" | sudo tee -a /etc/dhcpcd.conf
+    echo -e "\nsource $BM_ENV_PATH" >> ~/.bashrc
 fi
 
 DISABLE_BLUETOOTH=true
 if [[ "$DISABLE_BLUETOOTH" = true ]]; then
     # Disable bluetooth
     echo "
-
 # Disable Bluetooth
-dtoverlay=disable-bt" | sudo tee -a /boot/config.txt
+dtoverlay=disable-bt
+" | sudo tee -a /boot/config.txt
 
-    set +e
     sudo systemctl disable hciuart.service
     sudo systemctl disable bluetooth.service
 fi
 
-DISABLE_IPV6=true
-if [[ "$DISABLE_IPV6" = true ]]; then
-    # Disable IPv6 hostname resolution
-    sudo sed -i 's/use-ipv6=yes/use-ipv6=no/g' /etc/avahi/avahi-daemon.conf
-fi
-
-INSTALL_SERVER=true
-if [[ "$INSTALL_SERVER" = true ]]; then
-    # Install Apache, PHP and MySQL (MariaDB)
-    sudo apt -y install apache2 mariadb-server php php-mysql libapache2-mod-php
-
-    # Configure MySQL
-    sudo mysql_secure_installation
-
-    sudo adduser pi www-data
-    sudo adduser pi audio
-    chmod -R 755 $LINKED_SITE_ROOT
-    sudo ln -s $LINKED_SITE_ROOT $SITE_ROOT
-fi
-
-INSTALL_PICAM=true
-if [[ "$INSTALL_PICAM" = true ]]; then
-    # Install dependencies
-    sudo apt -y install libharfbuzz0b libfontconfig1
-
-    # Create directories and symbolic links
-    sudo install -d -o pi -g pi -m 755 $PICAM_PATH{,/archive} $BM_SHAREDMEM_PATH/{rec,hooks,state}
-
-    ln -sfn {$PICAM_PATH,$BM_SHAREDMEM_PATH/rec}/archive
-    ln -sfn {$BM_SHAREDMEM_PATH,$PICAM_PATH}/rec
-    ln -sfn {$BM_SHAREDMEM_PATH,$PICAM_PATH}/hooks
-    ln -sfn {$BM_SHAREDMEM_PATH,$PICAM_PATH}/state
-
-    sudo ln -s $BM_PICAM_OUTPUT_PATH $BM_PICAM_LINKED_OUTPUT_PATH
-
-    touch $PICAM_LOG_PATH
-
-    # Install picam binary
-    PICAM_VERSION=1.4.9
-    if [[ "$PICAM_VERSION" = "latest" ]]; then
-        DOWNLOAD_URL=$(curl https://api.github.com/repos/iizukanao/picam/releases/latest | grep browser_download_url | grep tar.xz | cut -d '"' -f 4)
-        FILENAME=$(echo $DOWNLOAD_URL | cut -d "/" -f 9)
-    else
-        FILENAME="picam-${PICAM_VERSION}-binary.tar.xz"
-        DOWNLOAD_URL="https://github.com/iizukanao/picam/releases/download/v${PICAM_VERSION}/$FILENAME"
-    fi
-    FILENAME_ROOT="${FILENAME%%.*}"
-    wget $DOWNLOAD_URL
-    tar xvf $FILENAME
-    mv $FILENAME_ROOT/picam $PICAM_PATH/
-    rm -r $FILENAME $FILENAME_ROOT
-fi
-
 SETUP_MIC=true
 if [[ "$SETUP_MIC" = true ]]; then
-    echo -e "export BM_MIC_ID=$(arecord -l | perl -n -e'/^card (\d+):.+, device (\d):.+$/ && print "hw:$1,$2"')\n" >> $BM_ENV_PATH
+    sudo adduser $USER audio
+    echo "export BM_MIC_ID=$(arecord -l | perl -n -e'/^card (\d+):.+, device (\d):.+$/ && print "hw:$1,$2"')" >> $BM_ENV_PATH
 fi
 
 INSTALL_BOOTSTRAP=true
@@ -178,9 +110,14 @@ if [[ "$INSTALL_BOOTSTRAP" = true ]]; then
         FILENAME="bootstrap-${BOOTSTRAP_VERSION}-dist.zip"
         DOWNLOAD_URL="https://github.com/twbs/bootstrap/releases/download/v${BOOTSTRAP_VERSION}/$FILENAME"
     fi
+    FILENAME_ROOT="${FILENAME%%.*}"
+    cd /tmp
     wget $DOWNLOAD_URL
-    unzip $FILENAME -d $SITE_ROOT/library/bootstrap
-    rm $FILENAME
+    unzip $FILENAME
+    mkdir -p $BM_LINKED_SITE_ROOT/library/bootstrap
+    mv $FILENAME_ROOT/* $BM_LINKED_SITE_ROOT/library/bootstrap/
+    rm  -r $FILENAME $FILENAME_ROOT
+    cd -
 fi
 
 INSTALL_VIDEOJS=true
@@ -195,14 +132,52 @@ if [[ "$INSTALL_VIDEOJS" = true ]]; then
         FILENAME="video-js-${VIDEOJS_VERSION}.zip"
         DOWNLOAD_URL="https://github.com/videojs/video.js/releases/download/v${VIDEOJS_VERSION}/$FILENAME"
     fi
+    cd /tmp
     wget $DOWNLOAD_URL
-    unzip $FILENAME -d $SITE_ROOT/library/video-js
+    mkdir -p $BM_LINKED_SITE_ROOT/library/video-js
+    unzip $FILENAME -d $BM_LINKED_SITE_ROOT/library/video-js
     rm $FILENAME
+    cd -
+fi
+
+INSTALL_PICAM=true
+if [[ "$INSTALL_PICAM" = true ]]; then
+    # Install dependencies
+    sudo apt -y install libharfbuzz0b libfontconfig1
+
+    # Create directories and symbolic links
+    sudo install -d -o $USER -g $GROUP -m $PERMISSIONS $BM_PICAM_PATH{,/archive} $BM_SHAREDMEM_PATH/{rec,hooks,state}
+
+    ln -sfn {$BM_PICAM_PATH,$BM_SHAREDMEM_PATH/rec}/archive
+    ln -sfn {$BM_SHAREDMEM_PATH,$BM_PICAM_PATH}/rec
+    ln -sfn {$BM_SHAREDMEM_PATH,$BM_PICAM_PATH}/hooks
+    ln -sfn {$BM_SHAREDMEM_PATH,$BM_PICAM_PATH}/state
+
+    sudo ln -s $BM_PICAM_OUTPUT_PATH $BM_PICAM_LINKED_OUTPUT_PATH
+
+    sudo touch $BM_PICAM_LOG_PATH
+    sudo chown $USER:$GROUP $BM_PICAM_LOG_PATH
+
+    # Install picam binary
+    PICAM_VERSION=1.4.9
+    if [[ "$PICAM_VERSION" = "latest" ]]; then
+        DOWNLOAD_URL=$(curl https://api.github.com/repos/iizukanao/picam/releases/latest | grep browser_download_url | grep tar.xz | cut -d '"' -f 4)
+        FILENAME=$(echo $DOWNLOAD_URL | cut -d "/" -f 9)
+    else
+        FILENAME="picam-${PICAM_VERSION}-binary.tar.xz"
+        DOWNLOAD_URL="https://github.com/iizukanao/picam/releases/download/v${PICAM_VERSION}/$FILENAME"
+    fi
+    FILENAME_ROOT="${FILENAME%%.*}"
+    cd /tmp
+    wget $DOWNLOAD_URL
+    tar -xvf $FILENAME
+    mv $FILENAME_ROOT/picam $BM_PICAM_PATH/
+    rm -r $FILENAME $FILENAME_ROOT
+    cd -
 fi
 
 SETUP_SERVICES=true
 if [[ "$SETUP_SERVICES" = true ]]; then
-    USER=www-data
     UNIT_DIR=/lib/systemd/system
     LINKED_UNIT_DIR=$BM_PATH/control/services
     SYSTEMCTL=/usr/bin/systemctl
@@ -217,7 +192,7 @@ After=mysqld.service
 [Service]
 Type=oneshot
 User=$USER
-Group=$USER
+Group=$GROUP
 ExecStart=$BM_PATH/control/startup.py
 
 [Install]
@@ -238,14 +213,49 @@ Description=Babymonitor $SERVICE service
 [Service]
 Type=simple
 User=$USER
-Group=$USER
+Group=$GROUP
 ExecStart=$BM_PATH/control/$SERVICE.py" > $LINKED_UNIT_DIR/$SERVICE_FILENAME
 
         sudo ln -sfn {$LINKED_UNIT_DIR,$UNIT_DIR}/$SERVICE_FILENAME
 
-        $CMD_ALIAS+=" $SYSTEMCTL stop $SERVICE_FILENAME, $SYSTEMCTL start $SERVICE_FILENAME, $SYSTEMCTL restart $SERVICE_FILENAME,"
+        CMD_ALIAS+=" $SYSTEMCTL stop $SERVICE_FILENAME, $SYSTEMCTL start $SERVICE_FILENAME, $SYSTEMCTL restart $SERVICE_FILENAME,"
     done
     echo -e "${CMD_ALIAS%,}\n%$USER ALL=BM_MODES\n" | sudo tee /etc/sudoers.d/$USER
+fi
+
+INSTALL_SERVER=true
+if [[ "$INSTALL_SERVER" = true ]]; then
+    # Install Apache, PHP and MySQL (MariaDB)
+    sudo apt -y install apache2 mariadb-server php php-mysql libapache2-mod-php
+
+    # Configure MySQL
+    sudo mysql_secure_installation
+
+    # Add main user to www-data group
+    sudo adduser $USER $GROUP
+
+    # Ensure permissions are correct in project folder
+    sudo chmod -R $PERMISSIONS $BM_PATH
+    sudo chown -R $USER:$GROUP $BM_PATH
+
+    # Link site folder to default Apache site root
+    sudo ln -s $BM_LINKED_SITE_ROOT $BM_SITE_ROOT
+
+    # Remove default site
+    sudo a2dissite 000-default
+    sudo rm -r /var/www/html
+    sudo rm -f /etc/apache2/sites-available/000-default.conf
+    sudo rm -f /etc/apache2/sites-available/default-ssl.conf
+
+    # Setup new site
+    SITE_NAME=$(basename $BM_SITE_ROOT)
+    echo "<VirtualHost *:80>
+	DocumentRoot $BM_SITE_ROOT
+	ErrorLog \${APACHE_LOG_DIR}/error.log
+	CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+" > /etc/apache2/sites-available/$SITE_NAME.conf
+    sudo a2ensite $SITE_NAME
 fi
 
 INITIALIZE_DATABASE=true
