c3a09714aa4173ad588a9dceb32bdf1058e81cbb
Lars Frogner <lars_frogner@hotmail.com>
2021-07-23 20:13:49 +0200

    Turn on error logging

--- site/config/error_config.php
+++ site/config/error_config.php
@@ -2,3 +2,6 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+require_once(__DIR__ . '/config.php');
+ini_set('log_errors', 1);
+ini_set('error_log', $_CONFIG['/tmp/php_errors.log']);
