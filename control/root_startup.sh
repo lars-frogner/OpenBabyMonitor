2af0b59b270f48aabd9f36e124a598a6412bf05f
Lars Frogner <lars_frogner@hotmail.com>
2021-07-24 23:37:33 +0200

    Call script for creating required folders in /run/shm on boot

--- control/root_startup.sh
+++ control/root_startup.sh
@@ -3,5 +3,7 @@
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 SERVER_CONTROL_DIR=$SCRIPT_DIR/../site/servercontrol
 
+$BM_PICAM_DIR/create_sharedmem_dirs.sh
+
 $SERVER_CONTROL_DIR/process_flag.sh
 $SERVER_CONTROL_DIR/monitor_flag.sh &
