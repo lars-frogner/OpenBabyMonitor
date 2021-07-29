482168b2f8922993c9f7d0c9d18d7458bcc28acd
Lars Frogner <lars_frogner@hotmail.com>
2021-07-29 02:13:11 +0200

    Add support for setting server time with a flag

--- site/servercontrol/monitor_flag.sh
+++ site/servercontrol/monitor_flag.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 FLAG_FILENAME=$(basename $BM_SERVER_ACTION_FILE)
--- site/servercontrol/process_flag.sh
+++ site/servercontrol/process_flag.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
@@ -7,18 +6,35 @@ AP_MODE_CMD=$SCRIPT_DIR/activate_ap_mode.sh
 CLIENT_MODE_CMD=$SCRIPT_DIR/activate_client_mode.sh
 REBOOT_CMD='systemctl reboot'
 SHUTDOWN_CMD='shutdown -h now'
+TIMESTAMP_CMD='date +%s -s @'
 
 AP_MODE_FLAG=activate_ap_mode
 CLIENT_MODE_FLAG=activate_client_mode
 REBOOT_FLAG=reboot
 SHUTDOWN_FLAG=shutdown
+TIMESTAMP_FLAG=set_timestamp
 
 if [[ ! -f "$BM_SERVER_ACTION_FILE" ]]; then
     exit 0
 fi
 
-FLAG=$(cat $BM_SERVER_ACTION_FILE)
-rm $BM_SERVER_ACTION_FILE
+CONTENT=$(cat $BM_SERVER_ACTION_FILE)
+rm -f $BM_SERVER_ACTION_FILE
+
+read -ra SPLITTED_CONTENT -d '' <<<"$CONTENT"
+FLAG="${SPLITTED_CONTENT[0]}"
+TIMESTAMP="${SPLITTED_CONTENT[1]}"
+
+if [[ ! -z "$TIMESTAMP" ]]; then
+    case $TIMESTAMP in
+      ''|*[!0-9]*)
+        echo "Invalid timestamp $TIMESTAMP" 1>&2
+        exit 1
+        ;;
+      *)
+        ;;
+    esac
+fi
 
 case $FLAG in
 
@@ -38,7 +54,12 @@ case $FLAG in
     eval $SHUTDOWN_CMD
     ;;
 
+  $TIMESTAMP_FLAG)
+    eval "${TIMESTAMP_CMD}${TIMESTAMP}"
+    ;;
+
   *)
     echo "Invalid flag $FLAG" 1>&2
+    exit 1
     ;;
 esac
