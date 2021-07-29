1f5d6a90c6b6180f5279ce88696df72536d0c979
Lars Frogner <lars_frogner@hotmail.com>
2021-07-29 02:16:58 +0200

    Separate environment variable assignments by semicolons

--- site/config/env_config.php
+++ site/config/env_config.php
@@ -5,7 +5,7 @@ require_once(SRC_PATH . '/io.php');
 $envvars = readLines(ENV_FILE_PATH);
 
 // Create string with environment variable assignment for prepending to commands in exec()
-define('ENVVAR_ASSIGNMENT', join(' ', $envvars) . ' ');
+define('ENVVAR_ASSIGNMENT', join('; ', $envvars) . '; ');
 
 // Include all environment variables in PHP scope
 foreach ($envvars as $assignment) {
