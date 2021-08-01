d61d71f021491e58b6c56ca12d738fa60b7969b6
Lars Frogner <lars_frogner@hotmail.com>
2021-08-01 02:00:53 +0200

    Add arguments

--- control/videostream.py
+++ control/videostream.py
@@ -14,19 +14,19 @@ def stream_video():
 
 
 def stream_video_with_settings(vertical_resolution=720,
+                               use_variable_framerate=True,
                                framerate=30,
-                               use_variable_framerate=False,
                                rotation=0,
                                flip_horizontally=False,
                                flip_vertically=False,
-                               exposure_mode='off',
+                               exposure_mode='auto',
                                metering='average',
                                exposure_value_compensation=0,
-                               exposure_time=None,
-                               iso=None,
+                               exposure_time=100,
+                               iso=400,
                                white_balance_mode='auto',
-                               red_gain=None,
-                               blue_gain=None,
+                               red_gain=0.0,
+                               blue_gain=0.0,
                                capture_audio=True,
                                volume=1.0,
                                show_time=True):
@@ -39,18 +39,47 @@ def stream_video_with_settings(vertical_resolution=720,
         'Vertical resolution ({}) is not one of {}'.format(
         vertical_resolution, ', '.join(list(HORIZONTAL_RESOLUTIONS.keys())))
     resolution_args = [
-        '-w',
-        str(HORIZONTAL_RESOLUTIONS[vertical_resolution]), '-h',
+        '--width',
+        str(HORIZONTAL_RESOLUTIONS[vertical_resolution]), '--height',
         str(vertical_resolution)
     ]
 
-    audio_args = ['--noaudio'] if not capture_audio else []
+    fps_args = ['--vfr'] if use_variable_framerate else [
+        '--fps', str(framerate)
+    ]
+
+    orientation_args = ['--rotation', str(rotation)]
+    if flip_horizontally:
+        orientation_args += ['--hflip']
+    if flip_vertically:
+        orientation_args += ['--vflip']
+
+    brightness_args = ['--metering', metering, '--ex', exposure_mode]
+    if exposure_mode == 'off':
+        brightness_args += [
+            '--evcomp',
+            str(exposure_value_compensation), '--shutter',
+            str(exposure_time), '--iso',
+            str(iso)
+        ]
+
+    color_args = ['--wb', white_balance_mode]
+    if white_balance_mode == 'off':
+        color_args += ['--wbred', str(red_gain), '--wbblue', str(blue_gain)]
+
+    audio_args = ['--alsadev', mic_id, '--volume',
+                  str(volume)] if capture_audio else ['--noaudio']
+
+    time_args = ['--time', '--timeformat', r'%a %d.%m.%Y %T'
+                 ] if show_time else []
+
+    output_args = ['--hlsdir', picam_output_path]
 
     with open(picam_log_path, 'a') as log_file:
-        subprocess.check_call([
-            os.path.join(picam_path, 'picam'), '-o', picam_output_path,
-            '--time', '--alsadev', mic_id
-        ] + resolution_args + audio_args,
+        subprocess.check_call([os.path.join(picam_path, 'picam')] +
+                              output_args + resolution_args + fps_args +
+                              orientation_args + brightness_args + color_args +
+                              audio_args + time_args,
                               stdout=log_file,
                               stderr=log_file,
                               cwd=picam_path)
