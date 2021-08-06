35ea2729c9295546f1bd8bc0730f7c66df27edc8
Lars Frogner <lars_frogner@hotmail.com>
2021-08-06 16:06:03 +0200

    Fix variables

--- site/servercontrol/monitor_flag.sh
+++ site/servercontrol/monitor_flag.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname $(readlink -f $0))
-FLAG_FILENAME=$(basename $BM_SERVER_ACTION_FILE)
+FLAG_FILENAME="$(basename $BM_SERVER_ACTION_FILE)"
 
 inotifywait -m -e create -e moved_to --format "%f" $BM_SERVER_ACTION_DIR \
     | while read FILENAME
         do
             if [[ "$FILENAME" = "$FLAG_FILENAME" ]]; then
-                $SCRIPT_DIR/process_flag.sh
+                $BM_SERVERCONTROL_DIR/process_flag.sh
             fi
         done
