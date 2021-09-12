039e6749e121c8f37f6bb14dab605bf1f370bc88
Lars Frogner <lars_frogner@hotmail.com>
2021-09-12 15:24:54 +0200

    Add newline

--- site/public/activate_ap_mode.php
+++ site/public/activate_ap_mode.php
@@ -1,6 +1,7 @@
 <?php
 require_once(dirname(__DIR__) . '/config/site_config.php');
 redirectIfLoggedOut('index.php');
+
 logout('index.php', function () {
   executeServerControlAction('activate_ap_mode');
 });
