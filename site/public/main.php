182a30ad3bc45061ac1ee218a7036212a7da5f81
Lars Frogner <lars_frogner@hotmail.com>
2021-10-29 12:24:01 +0200

    Fix wrong indentation

--- site/public/main.php
+++ site/public/main.php
@@ -95,136 +95,137 @@ require_once(TEMPLATES_DIR . '/main.php');
                 <use xlink:href="media/bootstrap-icons.svg#bullseye" />
               </svg>
             </div>
+          </div>
 
-            <div id="mode_content_audio" class="col w-100" <?php echo ($mode != MODE_VALUES['audiostream']) ? HIDDEN_STYLE : '' ?>>
-              <div class="row justify-content-center align-items-center">
-                <div class="col-sm-6 mb-5 text-center" <?php echo HIDDEN_STYLE ?>>
-                  <p id="mode_content_audio_error" class="alert alert-danger text-center"></p>
+          <div id="mode_content_audio" class="col w-100" <?php echo ($mode != MODE_VALUES['audiostream']) ? HIDDEN_STYLE : '' ?>>
+            <div class="row justify-content-center align-items-center">
+              <div class="col-sm-6 mb-5 text-center" <?php echo HIDDEN_STYLE ?>>
+                <p id="mode_content_audio_error" class="alert alert-danger text-center"></p>
+                <a class="btn btn-secondary" href="main.php">Forny siden</a>
+              </div>
+            </div>
+            <div class="row align-items-center justify-content-center" id="audiostream_visualization_mode_box" <?php echo HIDDEN_STYLE; ?>>
+              <div class="col-auto fw-bold">
+                Visualisering
+              </div>
+              <div class="col-auto">
+                <form>
+                  <div class="form-check form-check-inline">
+                    <input type="radio" class="form-check-input" name="audiostream_control_radio" id="audiostream_none_radio" autocomplete="off" onclick="switchAudioVisualizationModeTo(null);" checked>
+                    <label class="form-check-label" for="audiostream_none_radio">Ingen</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input type="radio" class="form-check-input" name="audiostream_control_radio" id="audiostream_time_radio" autocomplete="off" onclick="switchAudioVisualizationModeTo('time');">
+                    <label class="form-check-label" for="audiostream_time_radio">Bølge</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input type="radio" class="form-check-input" name="audiostream_control_radio" id="audiostream_frequency_radio" autocomplete="off" onclick="switchAudioVisualizationModeTo('frequency');">
+                    <label class="form-check-label" for="audiostream_frequency_radio">Frekvenser</label>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="row align-items-center justify-content-center" id="audiostream_fftsize_range_box" <?php echo HIDDEN_STYLE; ?>>
+              <div class="col-auto">
+                <label class="form-label" for="audiostream_fftsize_range">Målinger per bilde</label>
+              </div>
+              <div class="col-auto">
+                <div class="row flex-nowrap">
+                  <div class="col-10">
+                    <input type="range" class="form-range" value="7" min="5" max="13" step="1" id="audiostream_fftsize_range" oninput="$('#audiostream_fftsize_range_value').html(2**this.value); switchFFTSizePowerTo(this.value);">
+                  </div>
+                  <div class="col-1">
+                    <output id="audiostream_fftsize_range_value">128</output>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row justify-content-center" id="audiostream_canvas_box">
+            </div>
+            <div class="row justify-content-center" id="audiostream_player_box">
+            </div>
+          </div>
+
+          <div id="mode_content_video" class="h-100 px-0" <?php echo ($mode != MODE_VALUES['videostream']) ? HIDDEN_STYLE : '' ?>>
+            <div id="mode_content_video_box" class="h-100">
+            </div>
+          </div>
+
+          <div id="mode_content_standby" class="col-auto text-center" <?php echo ($mode != MODE_VALUES['standby']) ? HIDDEN_STYLE : '' ?>>
+            <svg class="bi mb-5 text-bm" style="width: 25vh; height: 25vh" fill="currentColor">
+              <use xlink:href="media/bootstrap-icons.svg#moon-fill" />
+            </svg>
+            <p class="mb-5 text-bm">Enheten er i hvilemodus</p>
+
+            <div class="row mb-3 align-items-center justify-content-center">
+              <div id="latency_container" class="col-auto text-center text-bm" style="min-width: 6em; display: none">
+                <svg id="latency_icon" class="bi" style="width: 2em; height: 2em" fill="currentColor">
+                  <use xlink:href="media/bootstrap-icons.svg#stopwatch" />
+                </svg>
+                <p id="latency_text" class="mb-0"></p>
+              </div>
+              <div class="col-auto text-center">
+                <p class="mb-0 text-bm">Forbindelse</p>
+                <button id="measure_bandwidth_button" class="btn btn-bm" data-toggle="tooltip" data-placement="top" title="Mål ned- og opplastningshastighet" disabled>
+                  <svg class="bi" style="width: 2.5em; height: 2.5em" fill="currentColor">
+                    <use xlink:href="media/bootstrap-icons.svg#speedometer2" />
+                  </svg>
+                  <p class="mb-0">Test</p>
+                </button>
+                <span id="measure_bandwidth_busy_spinner" class="spinner-border mt-3" <?php echo HIDDEN_STYLE; ?>></span>
+              </div>
+              <div id="download_speed_container" class="col-auto text-center text-bm" style="min-width: 6em; display: none">
+                <svg id="download_speed_icon" class="bi" style="width: 2em; height: 2em" fill="currentColor">
+                  <use xlink:href="media/bootstrap-icons.svg#cloud-arrow-down" />
+                </svg>
+                <p id="download_speed_text" class="mb-0"></p>
+              </div>
+            </div>
+
+            <div id="connection_progress_bar_container" class="progress" <?php echo HIDDEN_STYLE; ?>>
+              <div id="connection_progress_bar" class="progress-bar" style="width: 0%"></div>
+            </div>
+
+            <div class="row mb-5 align-items-start justify-content-center">
+              <p id="connection_results_message" class="mb-3 text-bm" <?php echo HIDDEN_STYLE; ?>>Tilgjengelige funksjoner</p>
+              <div id="connection_results_listen" class="col-auto text-center text-bm" <?php echo HIDDEN_STYLE; ?>>
+                <svg class="bi" style="width: 2em; height: 2em" fill="currentColor">
+                  <use xlink:href="media/bootstrap-icons.svg#bell-fill" />
+                </svg>
+                <p id="connection_results_listen_text" class="mb-0"></p>
+              </div>
+              <div id="connection_results_audio" class="col-auto text-center text-bm" <?php echo HIDDEN_STYLE; ?>>
+                <svg class="bi" style="width: 2em; height: 2em" fill="currentColor">
+                  <use xlink:href="media/bootstrap-icons.svg#mic-fill" />
+                </svg>
+                <p id="connection_results_audio_text" class="mb-0"></p>
+              </div>
+              <div id="connection_results_video" class="col-auto text-center text-bm" <?php echo HIDDEN_STYLE; ?>>
+                <svg class="bi" style="width: 2em; height: 2em" fill="currentColor">
+                  <use xlink:href="media/bootstrap-icons.svg#camera-video-fill" />
+                </svg>
+                <p id="connection_results_video_text" class="mb-0"></p>
+              </div>
+            </div>
+          </div>
+
+          <div id="mode_content_waiting" class="col-auto" <?php echo HIDDEN_STYLE; ?>>
+            <span class="spinner-grow text-bm"></span>
+          </div>
+
+          <div id="mode_content_error" class="col-auto" <?php echo HIDDEN_STYLE; ?>>
+            <div class="container">
+              <div class="row ">
+                <div class="col-sm-6 text-center">
+                  <p id="mode_content_error_message" class="alert alert-danger text-center"></p>
                   <a class="btn btn-secondary" href="main.php">Forny siden</a>
                 </div>
               </div>
-              <div class="row align-items-center justify-content-center" id="audiostream_visualization_mode_box" <?php echo HIDDEN_STYLE; ?>>
-                <div class="col-auto fw-bold">
-                  Visualisering
-                </div>
-                <div class="col-auto">
-                  <form>
-                    <div class="form-check form-check-inline">
-                      <input type="radio" class="form-check-input" name="audiostream_control_radio" id="audiostream_none_radio" autocomplete="off" onclick="switchAudioVisualizationModeTo(null);" checked>
-                      <label class="form-check-label" for="audiostream_none_radio">Ingen</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                      <input type="radio" class="form-check-input" name="audiostream_control_radio" id="audiostream_time_radio" autocomplete="off" onclick="switchAudioVisualizationModeTo('time');">
-                      <label class="form-check-label" for="audiostream_time_radio">Bølge</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                      <input type="radio" class="form-check-input" name="audiostream_control_radio" id="audiostream_frequency_radio" autocomplete="off" onclick="switchAudioVisualizationModeTo('frequency');">
-                      <label class="form-check-label" for="audiostream_frequency_radio">Frekvenser</label>
-                    </div>
-                  </form>
-                </div>
-              </div>
-              <div class="row align-items-center justify-content-center" id="audiostream_fftsize_range_box" <?php echo HIDDEN_STYLE; ?>>
-                <div class="col-auto">
-                  <label class="form-label" for="audiostream_fftsize_range">Målinger per bilde</label>
-                </div>
-                <div class="col-auto">
-                  <div class="row flex-nowrap">
-                    <div class="col-10">
-                      <input type="range" class="form-range" value="7" min="5" max="13" step="1" id="audiostream_fftsize_range" oninput="$('#audiostream_fftsize_range_value').html(2**this.value); switchFFTSizePowerTo(this.value);">
-                    </div>
-                    <div class="col-1">
-                      <output id="audiostream_fftsize_range_value">128</output>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="row justify-content-center" id="audiostream_canvas_box">
-              </div>
-              <div class="row justify-content-center" id="audiostream_player_box">
-              </div>
             </div>
-
-            <div id="mode_content_video" class="h-100 px-0" <?php echo ($mode != MODE_VALUES['videostream']) ? HIDDEN_STYLE : '' ?>>
-              <div id="mode_content_video_box" class="h-100">
-              </div>
-            </div>
-
-            <div id="mode_content_standby" class="col-auto text-center" <?php echo ($mode != MODE_VALUES['standby']) ? HIDDEN_STYLE : '' ?>>
-              <svg class="bi mb-5 text-bm" style="width: 25vh; height: 25vh" fill="currentColor">
-                <use xlink:href="media/bootstrap-icons.svg#moon-fill" />
-              </svg>
-              <p class="mb-5 text-bm">Enheten er i hvilemodus</p>
-
-              <div class="row mb-3 align-items-center justify-content-center">
-                <div id="latency_container" class="col-auto text-center text-bm" style="min-width: 6em; display: none">
-                  <svg id="latency_icon" class="bi" style="width: 2em; height: 2em" fill="currentColor">
-                    <use xlink:href="media/bootstrap-icons.svg#stopwatch" />
-                  </svg>
-                  <p id="latency_text" class="mb-0"></p>
-                </div>
-                <div class="col-auto text-center">
-                  <p class="mb-0 text-bm">Forbindelse</p>
-                  <button id="measure_bandwidth_button" class="btn btn-bm" data-toggle="tooltip" data-placement="top" title="Mål ned- og opplastningshastighet" disabled>
-                    <svg class="bi" style="width: 2.5em; height: 2.5em" fill="currentColor">
-                      <use xlink:href="media/bootstrap-icons.svg#speedometer2" />
-                    </svg>
-                    <p class="mb-0">Test</p>
-                  </button>
-                  <span id="measure_bandwidth_busy_spinner" class="spinner-border mt-3" <?php echo HIDDEN_STYLE; ?>></span>
-                </div>
-                <div id="download_speed_container" class="col-auto text-center text-bm" style="min-width: 6em; display: none">
-                  <svg id="download_speed_icon" class="bi" style="width: 2em; height: 2em" fill="currentColor">
-                    <use xlink:href="media/bootstrap-icons.svg#cloud-arrow-down" />
-                  </svg>
-                  <p id="download_speed_text" class="mb-0"></p>
-                </div>
-              </div>
-
-              <div id="connection_progress_bar_container" class="progress" <?php echo HIDDEN_STYLE; ?>>
-                <div id="connection_progress_bar" class="progress-bar" style="width: 0%"></div>
-              </div>
-
-              <div class="row mb-5 align-items-start justify-content-center">
-                <p id="connection_results_message" class="mb-3 text-bm" <?php echo HIDDEN_STYLE; ?>>Tilgjengelige funksjoner</p>
-                <div id="connection_results_listen" class="col-auto text-center text-bm" <?php echo HIDDEN_STYLE; ?>>
-                  <svg class="bi" style="width: 2em; height: 2em" fill="currentColor">
-                    <use xlink:href="media/bootstrap-icons.svg#bell-fill" />
-                  </svg>
-                  <p id="connection_results_listen_text" class="mb-0"></p>
-                </div>
-                <div id="connection_results_audio" class="col-auto text-center text-bm" <?php echo HIDDEN_STYLE; ?>>
-                  <svg class="bi" style="width: 2em; height: 2em" fill="currentColor">
-                    <use xlink:href="media/bootstrap-icons.svg#mic-fill" />
-                  </svg>
-                  <p id="connection_results_audio_text" class="mb-0"></p>
-                </div>
-                <div id="connection_results_video" class="col-auto text-center text-bm" <?php echo HIDDEN_STYLE; ?>>
-                  <svg class="bi" style="width: 2em; height: 2em" fill="currentColor">
-                    <use xlink:href="media/bootstrap-icons.svg#camera-video-fill" />
-                  </svg>
-                  <p id="connection_results_video_text" class="mb-0"></p>
-                </div>
-              </div>
-            </div>
-
-            <div id="mode_content_waiting" class="col-auto" <?php echo HIDDEN_STYLE; ?>>
-              <span class="spinner-grow text-bm"></span>
-            </div>
-
-            <div id="mode_content_error" class="col-auto" <?php echo HIDDEN_STYLE; ?>>
-              <div class="container">
-                <div class="row ">
-                  <div class="col-sm-6 text-center">
-                    <p id="mode_content_error_message" class="alert alert-danger text-center"></p>
-                    <a class="btn btn-secondary" href="main.php">Forny siden</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
           </div>
+
         </div>
+      </div>
     </main>
 
     <footer class="d-flex flex-grow-0 flex-shrink-1 justify-content-center">
