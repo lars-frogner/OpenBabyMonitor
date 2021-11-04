6a394571c204e94211af3992af5f8953cc05534a
Lars Frogner <lars_frogner@hotmail.com>
2021-11-04 23:54:29 +0100

    Add missing encryption key info

--- setup.sh
+++ setup.sh
@@ -134,7 +134,10 @@ if [[ "$SETUP_AUDIO" = true ]]; then
 rm -rf \$BM_AUDIO_STREAM_DIR
 install -d -o $BM_SERVER_USER -g $BM_WEB_GROUP -m $BM_READ_PERMISSIONS \$BM_AUDIO_STREAM_DIR
 openssl rand 16 > \$BM_AUDIO_STREAM_DIR/stream.key
-chown $BM_SERVER_USER:$BM_WEB_GROUP \$BM_AUDIO_STREAM_DIR/stream.key
+echo stream.key > \$BM_AUDIO_STREAM_DIR/stream.keyinfo
+echo stream.key >> \$BM_AUDIO_STREAM_DIR/stream.keyinfo
+echo $(openssl rand -hex 16) >> \$BM_AUDIO_STREAM_DIR/stream.keyinfo
+chown $BM_USER:$BM_WEB_GROUP \$BM_AUDIO_STREAM_DIR/stream.key
 chmod $BM_READ_PERMISSIONS \$BM_AUDIO_STREAM_DIR/stream.key
 " > $BM_DIR/control/prepare_audio_streaming.sh
     chmod $BM_READ_PERMISSIONS $BM_DIR/control/prepare_audio_streaming.sh
