ac90a5e7170f66cf7da9b5fdfb65eefb57190ed1
Lars Frogner <lars_frogner@hotmail.com>
2021-11-08 19:48:13 +0100

    Add prompt to repeat password

--- site/config/init/init_database.sh
+++ site/config/init/init_database.sh
@@ -3,11 +3,25 @@ set -e
 
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
-stty -echo
-printf "New site password: "
-read PASSWORD
-stty echo
-printf '\n'
+while : ; do
+    stty -echo
+    printf "New site password: "
+    read PASSWORD
+    stty echo
+    printf '\n'
+
+    stty -echo
+    printf "Repeat password: "
+    read PASSWORD_REPEAT
+    stty echo
+    printf '\n'
+
+    if [[ "$PASSWORD" != "$PASSWORD_REPEAT" ]]; then
+        echo 'The passwords did not match'
+    else
+        break
+    fi
+done
 
 sudo php $SCRIPT_DIR/init_database.php $PASSWORD
 if [[ "$?" == '0' ]]; then
