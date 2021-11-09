aa2a814cbd4e40e23e476eac4fe805c6af0c4eed
Lars Frogner <lars_frogner@hotmail.com>
2021-11-09 13:26:15 +0100

    Remove echo statements

--- site/servercontrol/set_ap_password.sh
+++ site/servercontrol/set_ap_password.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 NEW_PASSWORD="$1"
->&2 echo 'New password:'
->&2 echo $NEW_PASSWORD
->&2 echo 'SSID:'
->&2 echo $BM_NW_SSID
 
 NEW_PSK=$($BM_SERVERCONTROL_DIR/get_network_psk.sh "$BM_NW_SSID" "$NEW_PASSWORD")
->&2 echo $NEW_PSK
 sed -i "s/wpa_psk=.*/wpa_psk=$NEW_PSK/g" $BM_NW_AP_DIR/etc/hostapd/hostapd.conf
