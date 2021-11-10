a32693f698a7cd610122716ecea0753571f38606
Lars Frogner <lars_frogner@hotmail.com>
2021-11-11 00:45:53 +0100

    Add spacing for messages

--- site/public/server_settings.php
+++ site/public/server_settings.php
@@ -51,21 +51,21 @@ $connected_network = obtainConnectedNetworkSSID();
 
   <main id="main_container" style="display: none;">
     <?php if ($connection_succeeded === true) { ?>
-      <div class="d-flex flex-row justify-content-center">
+      <div class="d-flex flex-row justify-content-center mt-3">
         <div class="d-flex flex-column">
           <span class="alert alert-success text-center"><?php echo LANG['connection_succeeded']; ?></span>
         </div>
       </div>
     <?php } ?>
     <?php if ($connection_succeeded === false) { ?>
-      <div class="d-flex flex-row justify-content-center">
+      <div class="d-flex flex-row justify-content-center mt-3">
         <div class="d-flex flex-column">
           <span class="alert alert-danger text-center"><?php echo LANG['connection_failed']; ?></span>
         </div>
       </div>
     <?php } ?>
     <div style="display: none;" id="switching_network_info">
-      <div class="d-flex flex-row justify-content-center">
+      <div class="d-flex flex-row justify-content-center mt-3">
         <div class="d-flex flex-column text-center">
           <span class="alert alert-warning"><?php echo LANG['switching_network']; ?></span>
           <a class="btn btn-secondary" href="server_settings.php"><?php echo LANG['refresh']; ?></a>
@@ -73,7 +73,7 @@ $connected_network = obtainConnectedNetworkSSID();
       </div>
     </div>
     <?php if ($password_changed === true) { ?>
-      <div class="d-flex flex-row justify-content-center">
+      <div class="d-flex flex-row justify-content-center mt-3">
         <div class="d-flex flex-column">
           <span class="alert alert-success text-center"><?php echo LANG[$which_password . '_password_changed']; ?></span>
         </div>
