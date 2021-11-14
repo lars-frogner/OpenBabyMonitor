319087f57f2a4d0d6e7495b855f71d2d96111845
Lars Frogner <lars_frogner@hotmail.com>
2021-11-15 00:20:22 +0100

    Add log entry when switching to AP mode

--- site/servercontrol/ensure_connection.sh
+++ site/servercontrol/ensure_connection.sh
@@ -10,6 +10,7 @@ if [[ "$ACCESS_POINT_ACTIVE" = "0" ]]; then
     CONNECTED_TO_NETWORK_SECOND=$($BM_SERVERCONTROL_DIR/connected_to_external_network.sh)
 
     if [[ "$CONNECTED_TO_NETWORK_FIRST" = "0" && "$CONNECTED_TO_NETWORK_SECOND" = "0" ]]; then
+        echo 'Connection lost, activating access point mode' >> $BM_SERVER_LOG_PATH
         $BM_SERVERCONTROL_DIR/perform_action.sh activate_ap_mode
     fi
 fi
