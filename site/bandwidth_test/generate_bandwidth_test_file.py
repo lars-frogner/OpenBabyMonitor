a8c66c0456bd1d202fd4e8a896a8a62de7cce1d5
Lars Frogner <lars_frogner@hotmail.com>
2021-11-16 22:09:35 +0100

    Remove dependence on environment variable

--- site/bandwidth_test/generate_bandwidth_test_file.py
+++ site/bandwidth_test/generate_bandwidth_test_file.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 
 size = int(sys.argv[1])  # [kB]
-file_path = f'{os.environ["BM_DIR"]}/site/bandwidth_test/test_data.bin'
+file_path = f'{os.path.abspath(os.path.dirname(__file__))}/test_data.bin'
 
 np.zeros(size * 1000, np.uint8).tofile(file_path)
 os.chmod(file_path, 0o750)
