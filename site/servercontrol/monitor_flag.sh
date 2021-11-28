4186d1af0d0a1f4baea39be06bcee352dfd99386
Lars Frogner <lars_frogner@hotmail.com>
2021-11-28 15:27:40 +0100

    Make inotifywait more quiet

--- site/servercontrol/monitor_flag.sh
+++ site/servercontrol/monitor_flag.sh
@@ -2,7 +2,7 @@
 
 FLAG_FILENAME="$(basename $BM_SERVER_ACTION_FILE)"
 
-inotifywait -m -e create -e moved_to --format "%f" $BM_SERVER_ACTION_DIR \
+inotifywait -q -m -e create -e moved_to --format "%f" $BM_SERVER_ACTION_DIR \
     | while read FILENAME
         do
             if [[ "$FILENAME" = "$FLAG_FILENAME" ]]; then
