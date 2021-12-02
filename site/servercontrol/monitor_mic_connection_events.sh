b671a8fab3bfee62373f997fefee0005e4f94050
Lars Frogner <lars_frogner@hotmail.com>
2021-12-03 00:13:37 +0100

    Remove `echo`

--- site/servercontrol/monitor_mic_connection_events.sh
+++ site/servercontrol/monitor_mic_connection_events.sh
@@ -5,7 +5,6 @@ udevadm monitor --udev --subsystem-match=sound/pcm \
         do
             EVENT=$(echo $LINE | sed -n 's/^UDEV \[[0-9\.]*\.[0-9\.]*\] \([a-z]*\) .*$/\1/p')
             if [[ ! -z "$EVENT" ]]; then
-                echo '!'
                 $BM_SERVERCONTROL_DIR/auto_select_mic.sh
             fi
         done
