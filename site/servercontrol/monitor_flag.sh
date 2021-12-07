c7bad2481597b466151fe3b9d7108dd7febbd6ea
Lars Frogner <lars_frogner@hotmail.com>
2021-12-07 14:52:57 +0100

    Prevent server control actions from being performed at the same time

--- site/servercontrol/monitor_flag.sh
+++ site/servercontrol/monitor_flag.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+source $BM_SERVERCONTROL_DIR/redirection.sh
 
 FLAG_FILENAME="$(basename $BM_SERVER_ACTION_FILE)"
 
-inotifywait -q -m -e create -e moved_to --format "%f" $BM_SERVER_ACTION_DIR \
+inotifywait -q -m -e create -e moved_to --format "%f" $BM_SERVER_ACTION_DIR 2>&4 \
     | while read FILENAME
         do
             if [[ "$FILENAME" = "$FLAG_FILENAME" ]]; then
-                $BM_SERVERCONTROL_DIR/process_flag.sh
+                flock --exclusive --timeout 30 $BM_SERVER_ACTION_LOCK_FILE -c "$BM_SERVERCONTROL_DIR/process_flag.sh" 2>&4
             fi
         done
--- site/servercontrol/monitor_mic_connection_events.sh
+++ site/servercontrol/monitor_mic_connection_events.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+source $BM_SERVERCONTROL_DIR/redirection.sh
 
-udevadm monitor --udev --subsystem-match=sound/pcm \
+udevadm monitor --udev --subsystem-match=sound/pcm 2>&4 \
     | while read LINE
         do
-            EVENT=$(echo $LINE | sed -n 's/^UDEV \[[0-9\.]*\.[0-9\.]*\] \([a-z]*\) .*$/\1/p')
+            EVENT=$(echo $LINE | sed -n 's/^UDEV \[[0-9\.]*\.[0-9\.]*\] \([a-z]*\) .*$/\1/p'  2>&4)
             if [[ ! -z "$EVENT" ]]; then
-                $BM_SERVERCONTROL_DIR/auto_select_mic.sh
+                flock --exclusive --timeout 30 $BM_SERVER_ACTION_LOCK_FILE -c "$BM_SERVERCONTROL_DIR/auto_select_mic.sh" 2>&4
             fi
         done
