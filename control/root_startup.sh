b339adf0ac9604baec71c115ab2db728b8f09ea4
Lars Frogner <lars_frogner@hotmail.com>
2021-12-26 22:32:28 +0100

    Make sure `rm` does not fail when file is missing

--- control/root_startup.sh
+++ control/root_startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm $BM_CONTROL_TIME_SYNCED_FILE
+rm -f $BM_CONTROL_TIME_SYNCED_FILE
 $BM_SERVERCONTROL_DIR/inform_camera_connected.sh
 $BM_SERVERCONTROL_DIR/process_flag.sh
 $BM_SERVERCONTROL_DIR/monitor_flag.sh &
