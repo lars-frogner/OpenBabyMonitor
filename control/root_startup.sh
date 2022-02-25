cb82f201eb5ed891c9c37172b50f01bb5199fb96
Lars Frogner <lars_frogner@hotmail.com>
2022-02-25 21:39:54 +0100

    Fix failure to reset mode at startup due to insufficient permissions when calling `mic.py`

--- control/root_startup.sh
+++ control/root_startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -f $BM_CONTROL_TIME_SYNCED_FILE
+$BM_DIR/control/mic.py --select-mic --auto-choice
 $BM_SERVERCONTROL_DIR/inform_camera_connected.sh
 $BM_SERVERCONTROL_DIR/process_flag.sh
 $BM_SERVERCONTROL_DIR/monitor_flag.sh &
--- control/startup.py
+++ control/startup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
 if __name__ == '__main__':
-    import mic
     import standby
-    mic.select_mic(auto_choice=True)
     standby.set_standby()
